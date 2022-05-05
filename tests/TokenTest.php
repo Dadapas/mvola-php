64850bd31758aadbff839a74a01b5faf653c484c
TOVOHERY <tovoherypascal@gmail.com>
2022-05-05 16:19:10 +0300

    Tests

---
+++ tests/TokenTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace MobileMoney\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use MobileMoney\Token;
+
+final class TokenTest extends TestCase
+{
+    public function testToken()
+    {
+        
+        $this->assertSame(1, 1);        
+    }
+}
