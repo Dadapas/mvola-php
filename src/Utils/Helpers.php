69cdc4ae215afa27810f0350fe4b5f890f42b42d
TOVOHERY <tovoherypascal@gmail.com>
2022-05-11 15:43:03 +0300

    (fixed) uuid for ramsey/uuid

--- src/Utils/Helpers.php
+++ src/Utils/Helpers.php
@@ -2,6 +2,9 @@
 
 namespace MVolaphp\Utils;
 
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
+
 class Helpers
 {
 	private static $alphabet = "abcdefghijklmnopqrstvuxwyz0123456789";
@@ -47,16 +50,41 @@ class Helpers
 		return $str;
 	}
 
+	/**
+	 * Gererate UUID with php
+	 * 
+	 * solution from
+	 * https://www.uuidgenerator.net/dev-corner/php
+	*/ 
+	private static function uuidNative()
+	{
+		// Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+	    $data = $data ?? \random_bytes(16);
+	    \assert(strlen($data) == 16);
+
+	    // Set version to 0100
+	    $data[6] = \chr(\ord($data[6]) & 0x0f | 0x40);
+	    // Set bits 6-7 to 10
+	    $data[8] = \chr(\ord($data[8]) & 0x3f | 0x80);
+
+	    // Output the 36 character UUID.
+	    return \vsprintf('%s%s-%s-%s-%s-%s%s%s', \str_split(\bin2hex($data), 4));
+	}
+
 	public static function uuid()
 	{
-		$str  = self::one(8) . "-";
-		$str .= self::one(4) . "-";
-		$str .= self::one(4) . "-";
-		$str .= self::one(4) . "-";
-		$str .= self::one(12);
-		
-		return $str;
+		try {
+
+			$uuid1 = Uuid::uuid4();
+			
+			return $uuid1->toString();
+
+		} catch (UnsatisfiedDependencyException $e) {
+
+			return self::uuidNative();
+		}
 	}
+
 
 	public static function ref()
 	{
